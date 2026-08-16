3634dca7a5203c4b434b7a9ee7451b1d3138d122
7Limes <milesb2288@gmail.com>
2026-08-16 10:20:17 -0400

    Add to stdlib

--- src/stdlib/stdlib.c
+++ src/stdlib/stdlib.c
@@ -1,7 +1,10 @@
 #include "../util/map.h"
 
 #include "color.h"
+#include "font.h"
 #include "graphics.h"
+#include "math.h"
+#include "string.h"
 
 Map STDLIB_LOOKUP = {0};
 
@@ -11,5 +14,8 @@ void init_stdlib_lookup() {
     map_create(m, 64);
 
     map_add(m, ".color", stdlib_color);
+    map_add(m, ".font", stdlib_font);
     map_add(m, ".graphics", stdlib_graphics);
+    map_add(m, ".math", stdlib_math);
+    map_add(m, ".string", stdlib_string);
 }
